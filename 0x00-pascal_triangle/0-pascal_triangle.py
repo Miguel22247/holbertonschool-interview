c1d16a54c7d739485c598128f141b9299e9755ff
Miguel Pacheco <minipachru@gmail.com>
2021-11-30 16:42:17 -0300

    add codebase

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Pascal Triangle """
+
+
+def pascal_triangle(n):
+    """calculates pascal and returns a list"""
+    pascal_tri_list = []
+
+    if n <= 0:
+        return pascal_tri_list
+
+    for i in range(n):
+        temp_pascal_list = []
+        for j in range(i + 1):
+            if (j == 0 or j == i):
+                temp_pascal_list.append(1)
+            else:
+                temp_pascal_list.append(pascal_tri_list[i-1][j] + pascal_tri_list[i-1][j])
+        pascal_tri_list.append(temp_pascal_list)
+    return pascal_tri_list
+ 
