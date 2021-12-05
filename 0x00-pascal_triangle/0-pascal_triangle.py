57886174be62d5f9760b6afcd9815befc3cb4bf4
Miguel Pacheco <minipachru@gmail.com>
2021-12-05 17:26:36 -0300

    fix pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,10 +4,10 @@
 
 def pascal_triangle(n):
     """calculates pascal and returns a list"""
-    pascal_tri_list = []
+    pascal_list = []
 
     if n <= 0:
-        return pascal_tri_list
+        return pascal_list
 
     for i in range(n):
         temp_list = []
@@ -15,6 +15,6 @@ def pascal_triangle(n):
             if (j == 0 or j == i):
                 temp_list.append(1)
             else:
-                temp_list.append(pascal_tri_list[i-1][j-1] + pascal_tri_list[i-1][j])
-        pascal_tri_list.append(temp_list)
-    return pascal_tri_list
+                temp_list.append(pascal_list[i-1][j-1] + pascal_list[i-1][j])
+        pascal_list.append(temp_list)
+    return pascal_list
