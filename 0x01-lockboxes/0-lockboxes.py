9110aaf8b28b6f64846458c8cebe0811b1c79ffd
Miguel Pacheco <minipachru@gmail.com>
2021-12-08 11:49:24 -0300

    updated code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,28 +4,33 @@
 
 def canUnlockAll(boxes):
     """ method that verifies unlocking """
-    if type(boxes) is not list or not all(type(box) is list):
-        for (box in boxes):
-            return False
+    if type(boxes) is not list or not all(type(box) is list for box in boxes):
+        return False
+    # check if list is empty
     if len(boxes) == 0:
         return False
+    # check if only exist one box
     if len(boxes) == 1:
         return True
+    # check if first box is empty
     if not boxes[0] and len(boxes) > 1:
         return False
+    # dictionary of all boxes, all boxes are lock here
     unlock = {k: False for k in range(len(boxes))}
+    # unlock first box
     unlock[0] = True
+    # List of all key's first box
     keys = [key for key in boxes[0]]
     # Process of unlock boxes
     while keys:
-        new_k = []
+        new_key = []
         for key in keys:
             if key in unlock.keys() and unlock[key] is False:
-                new_k += boxes[key]
+                new_key += boxes[key]
                 unlock[key] = True
         # If all boxes unlock return True
         if all(unlock.values()) and len(unlock) == len(boxes):
             return True
         # Change keys for the new keys to check
-        keys = new_k
+        keys = new_key
     return False
