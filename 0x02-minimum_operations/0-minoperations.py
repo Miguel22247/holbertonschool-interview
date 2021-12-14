5aa528301f4a8a206eb1567fd73eb1ae91acfdd5
Miguel Pacheco <minipachru@gmail.com>
2021-12-14 09:58:32 -0300

    add task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,5 +5,24 @@
 def minOperations(n):
     """given a number n, this method calculates the fewest name of operations"""
 
+    CopyPaste = 2
+    Paste = 1
+    Htotal = 1
+    Previoush = 1
+    numofops = 0
+
     if n <= 1:
         return 0
+
+    while Htotal < n:
+        if Htotal == 1:
+            numofops += CopyPaste
+            Htotal = Htotal + Htotal
+        elif n % Htotal == 0 and Htotal * 2 <= n:
+            numofops += CopyPaste
+            Previoush = Htotal
+            Htotal = Htotal + Htotal
+        else:
+            Htotal += Previoush
+            numofops += Paste
+    return numberOfOperationsaste
