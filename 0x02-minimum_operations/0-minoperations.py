7955c5add95b04826e21d921d2ba4f0ed33002fa
Miguel Pacheco <minipachru@gmail.com>
2021-12-14 08:56:25 -0300

    add file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""FIle that uses the min of operations"""
+
+
+def minOperations(n):
+    """given a number n, this method calculates the fewest name of operations"""
+
+    if n <= 1:
+        return 0
