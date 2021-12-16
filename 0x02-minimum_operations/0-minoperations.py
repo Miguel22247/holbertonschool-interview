f7aa4a8a7f5cef698ceef79507b07edb1feaaa85
Miguel Pacheco <minipachru@gmail.com>
2021-12-16 08:24:37 -0300

    rewrite code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,28 +3,14 @@
 
 
 def minOperations(n):
-  """
-  given a number n, this method calculates the fewest name of operations
-  """
-
-  copyPaste = 2
-  Paste = 1
-  numofops = 0
-  totalH = 1
-  hPrev = 1
-
-  if n <= 1:
-      return 0
-
-  while totalH < n:
-      if totalH == 1:
-          numofops += copyPaste
-          totalH = totalH + totalH
-      elif n % totalH == 0 and totalH * 2 <= n:
-          numofops += copyPaste
-          hPrev = totalH
-          totalH = totalH + totalH
-      else:
-          totalH += hPrev
-          numofops += Paste
-  return numofops
+    """
+    given a number n, this method calculates the fewest name of operations
+    """
+    minOps = 0
+    divisor = 2
+    while (isinstance(n, int) and n > 1):
+        while (n % divisor):
+            divisor += 1
+        minOps += divisor
+        n = n // divisor
+    return minOps
