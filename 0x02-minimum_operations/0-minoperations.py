574e586b40e7477d2ebc5b0bc1a31686525730af
Miguel Pacheco <minipachru@gmail.com>
2021-12-16 08:30:56 -0300

    fix pep8

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,27 +3,27 @@
 
 
 def minOperations(n):
-  """
-  given a number n, this method calculates the fewest name of operations
-  """
-  copyPaste = 2
-  Paste = 1
-  numofops = 0
-  totalH = 1
-  hPrev = 1
+    """
+    given a number n, this method calculates the fewest name of operations
+    """
+    copyPaste = 2
+    Paste = 1
+    numofops = 0
+    totalH = 1
+    hPrev = 1
 
-  if n <= 1:
-        return 0
+    if n <= 1:
+            return 0
 
-  while totalH < n:
-        if totalH == 1:
-            numofops += copyPaste
-            totalH = totalH + totalH
-        elif n % totalH == 0 and totalH * 2 <= n:
-            numofops += copyPaste
-            hPrev = totalH
-            totalH = totalH + totalH
-        else:
-            totalH += hPrev
-            numofops += Paste
-  return numofops
+    while totalH < n:
+            if totalH == 1:
+                numofops += copyPaste
+                totalH = totalH + totalH
+            elif n % totalH == 0 and totalH * 2 <= n:
+                numofops += copyPaste
+                hPrev = totalH
+                totalH = totalH + totalH
+            else:
+                totalH += hPrev
+                numofops += Paste
+    return numofops
