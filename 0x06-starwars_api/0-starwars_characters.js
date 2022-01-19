5198e98c1d6895b31d806ce2e1115890825ba7da
Miguel Pacheco <minipachru@gmail.com>
2022-01-19 09:05:45 -0300

    add task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
-request(url, function (error, response, body){
-  const film = (JSON.parse(body.body))
-  // added for debugging
+request.get(url, function (response, body) {
+  const film = (JSON.parse(body.body));
+  // debugging
   // console.log(film);
   // console.log(film.characters);
-  character(film.character);
+  character(film.characters);
 });
-
 const character = (people, i = 0) => {
   if (i === people.length) return;
   request.get(people[i], function (response, body) {
