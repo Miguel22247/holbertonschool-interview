59d03fc0fb4ed37dcbffe8381a46f10a6a6cae87
Miguel Pacheco <minipachru@gmail.com>
2022-02-01 09:34:50 -0300

    add main testing file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,4 +3,24 @@
 
 def makeChange(coins, total):
     """determine the fewest number of coins"""
-     if total 
+     if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    i, number_coins = (0, 0)
+    len_coins = len(coins)
+    cpy_total = total
+
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            number_coins +=1
+        else:
+            i += 1
+
+    checker = cpy_total > 0 and number_coins > 0
+    if checker or number == 0:
+        return -1
+    else:
+        return number_coins
