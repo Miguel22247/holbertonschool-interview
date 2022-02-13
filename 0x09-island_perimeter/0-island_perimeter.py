1d77f4720a79efd045402111047baa59f048cec3
Miguel Pacheco <minipachru@gmail.com>
2022-02-13 19:24:47 -0300

    fix compile errors

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,19 +3,23 @@
 
 
 def island_perimeter(grid):
-    """Land is 1 and water is 0"""
+    """land is 1 and water is 0"""
     land = 1
     water = 0
     perimeter = 0
     for y, row in enumerate(grid):
         for x, cell in enumerate(row):
             if cell == land:
-                if y == 0 or grid[y + 1][x] == water:
+                # print("land in [x= {:d} y= {:d}]".format(x, y))
+                # left
+                if y == 0 or grid[y - 1][x] == water:
                     perimeter += 1
-                if y == len(grid) - 1 or grid[y + 1][x] == WATER:
+                # right
+                if y == len(grid) - 1 or grid[y + 1][x] == water:
                     perimeter += 1
-                if x == 0 or grid[y][x - 1] == WATER:
+                # up
+                if x == 0 or grid[y][x - 1] == water:
                     perimeter += 1
-                if x == len(row) - 1 or grid[y][x + 1] == WATER:
-                    perimeter += 1
-    return perimeter
+                # down
+                if x == len(row) - 1 or grid[y][x + 1] == water:
+                    per
