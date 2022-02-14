61d3dbe249b2ca4d545e1796ce70e2e971fbbd51
Miguel Pacheco <minipachru@gmail.com>
2022-02-14 09:41:07 -0300

    add prototype

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 """Given consecutive integers starting from 1 up to and including n"""
+
+
+def isWinner(x, nums):
+    """Given consecutive integers starting from 1 up to and including n"""
